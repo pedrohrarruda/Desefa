89f681489bbdc24be349ffbc26f685ebabfd6c58
Gaudiot <victor.gaudiot@gmail.com>
2022-04-07 02:13:46 -0300

    Created base class to the pieces

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public abstract class Piece : MonoBehaviour{
+    private bool moveOnce = false;
+    private bool firstMove = true;
+
+    private List<Vector2Int> movement = new List<Vector2Int>();
+    private Vector2Int position = new Vector2Int();
+    private int team; //white(+1), black(-1)
+
+    public virtual List<Vector2Int> GetAvailableMoves(ref Board board){
+        List<Vector2Int> moves = new List<Vector2Int>();
+
+        for(int i = 0 ; i < movement.Count ; i++){
+            Vector2Int pos = position;
+
+            while(true){
+                pos = pos + movement[i];
+
+                if(board.PieceCanOccupy(position) == false) break;
+                
+                Piece piece = board.GetPiece(pos);
+
+                if(piece == null || piece.team != this.team) moves.Add(pos);
+                if(piece != null) break;
+
+                if(moveOnce) break;
+            }
+        }
+
+        return moves;
+    }
+
+    public void MoveTo(Vector2Int new_position){
+        firstMove = false;
+        position = new_position;
+        this.transform.position = new Vector3(position.x, position.y);
+    }
+}
