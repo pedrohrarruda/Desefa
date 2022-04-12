0d8372690446d620238fb45b96a77dd7334333a7
Maria Augusta <mamb2@cin.ufpe.br>
2022-04-12 12:02:10 -0300

    define winner when time is up

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance { get; private set; }
+    private void Awake(){
+        if(Instance != null && Instance != this){
+            Destroy(this);
+        }else{
+            Instance = this;
+        }
+    }
+
     public enum TurnPlayer {white, black};
     private TurnPlayer turnPlayer;
 
     public CountdownTimer timerWhite, timerBlack;
 
-    
+    public void TurnPlayerWins(){
+        //TODO
+        Debug.Log(turnPlayer + " wins the game.");
+    }
+
+    public void TurnPlayerLose(){
+        turnPlayer = (turnPlayer == TurnPlayer.white) ? TurnPlayer.black : TurnPlayer.white;
+        TurnPlayerWins();
+    }
 
     public void SwitchTurn(){
         turnPlayer = (turnPlayer == TurnPlayer.white) ? TurnPlayer.black : TurnPlayer.white;
